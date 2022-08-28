0765719709261470f13794d016fdf7f75d71b6b1
sinhasoumyadeep1234 <110376227+sinhasoumyadeep1234@users.noreply.github.com>
2022-08-28 10:31:15 +0530

    Update ChangeCase.java

--- ChangeCase.java
+++ ChangeCase.java
@@ -1,33 +1,41 @@
 import java.lang.*;
 import java.util.*;
 
-class ChangeCase {
+class ChangeCase1 {
     Scanner sc;    
     public static void main(String[] args) {    
-        Scanner sc = new Scanner(System.in);
+         Scanner sc = new Scanner(System.in);
 	   System.out.println("Enter your string:");
-	   String str1=sc.nextLine();    
-        StringBuffer newStr=new StringBuffer(str1);    
-            
-        for(int i = 0; i < str1.length(); i++) {
+	   String str1=sc.nextLine();
+	   ChangeVowelCase change = new ChangeVowelCase();
+	   change.changeCase(str1);     
+             
+         }    
+}
+class ChangeVowelCase
+{
+	public void changeCase(String str1)
+	{
+	StringBuffer newStr=new StringBuffer(str1);
+	for(int i = 0; i < str1.length(); i++) {
 		      
-            char ch=str1.charAt(i);
+              char ch=str1.charAt(i);
 		  if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' || ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U')	
 		  {
                 
-           	 if(Character.isLowerCase(str1.charAt(i))) 
-			 {    
+           	 	if(Character.isLowerCase(str1.charAt(i))) 
+			{    
                     
-                	newStr.setCharAt(i, Character.toUpperCase(str1.charAt(i)));    
-            	 }    
+                		newStr.setCharAt(i, Character.toUpperCase(str1.charAt(i)));    
+            	}    
                 
-            	 else if(Character.isUpperCase(str1.charAt(i))) 
-			 {    
+            	else if(Character.isUpperCase(str1.charAt(i))) 
+			{    
                     
-                	newStr.setCharAt(i, Character.toLowerCase(str1.charAt(i)));    
-                }
+                		newStr.setCharAt(i, Character.toLowerCase(str1.charAt(i)));    
+                  }
 		  }    
         }    
-        System.out.println("String after case conversion : " + newStr);    
-    }    
-}
+        System.out.println("String after case conversion : " + newStr);
+	}    
+}    
