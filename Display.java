94c8b05e717d37c10f87fb85748afea108901d1c
sinhasoumyadeep1234 <110376227+sinhasoumyadeep1234@users.noreply.github.com>
2022-08-28 10:34:57 +0530

    Add files via upload
    Simple java program to display the elements present in an array.

---
+++ Display.java
@@ -0,0 +1,9 @@
+class Display{
+	public static void main(String args[]){
+		int []arr =new int[]{1,2,3,4,5};
+		System.out.println(“Elements of the array are:”);
+		for(int i=0;i<arr.length;i++){
+			System.out.print(arr[i] + “ ”);
+		}
+	}
+}
